1b50f78120bdfc9b1378c1c634cefcc90c40e9ec
KazaJhodo <kazajhodo@ashenrayne.com>
2023-07-11 13:13:02 -0500

    Fixed exec-host comment on solr removal.

--- scripts/setBaseConfiguration.sh
+++ scripts/setBaseConfiguration.sh
@@ -4,7 +4,7 @@ removeSolr() {
   if [[ $1 == 'n' || $1 == 'no' ]]; then
     rm -rf .ddev/solr .ddev/docker-compose.solr.yaml
     sed -i '' "s/post-start:/# post-start:/" .ddev/config.yaml >/dev/null
-    sed -i '' "s/  - exec-host: ddev solrcollection/#  -exec-host: ddev solrcollection/" .ddev/config.yaml >/dev/null
+    sed -i '' "s/  -exec-host: ddev solrcollection/#  -exec-host: ddev solrcollection/" .ddev/config.yaml >/dev/null
     echo 'Solr removed from ddev.'
   elif [[ $1 == 'y' || $1 == 'yes' ]]; then
     echo "You're all set."
@@ -20,7 +20,6 @@ if ! grep -q "\[client-code\]" .ddev/config.yaml; then
   exit 0
 fi
 
-echo
 echo 'Client code?'
 read client_code
 sed -i '' "s/\[client-code\]/$client_code/" .ddev/config.yaml >/dev/null
