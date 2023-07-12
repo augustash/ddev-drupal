adecfb6f8bfcc4b59762efbb055720e3bfd63ddf
KazaJhodo <kazajhodo@ashenrayne.com>
2023-07-12 13:31:03 -0500

    Fix exec host syntax error.

--- assets/ddev.php
+++ assets/ddev.php
@@ -34,7 +34,7 @@ class Ddev {
 
         $solr = readline("Would in like solr installed?(y/n)\n");
         if ($solr == 'yes' || $solr == 'y') {
-          $content = str_replace('# -exec-host: ddev solrcollection', '  -exec-host: ddev solrcollection', $content);
+          $content = str_replace('# - exec-host: ddev solrcollection', '  - exec-host: ddev solrcollection', $content);
           if (file_put_contents($configuration, $content)) {
             // Add docker-compose.solr.yaml and solr directory from .ddev/assets.
             shell_exec('mv ' . __DIR__ . '/../../.ddev/assets/solr ' . __DIR__ . '/../../.ddev');
