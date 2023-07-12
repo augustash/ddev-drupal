e54a4f849c8217e79ce9eedad7723b6431319574
KazaJhodo <kazajhodo@ashenrayne.com>
2023-07-12 17:37:21 -0500

    We’re getting creative.
    Check if the ddev command exists. This will work locally, but always fail on a host environment.

--- scripts/ddev.php
+++ scripts/ddev.php
@@ -7,7 +7,7 @@ use Composer\Script\Event;
 class Ddev {
     public static function postPackageInstall(Event $event) {
       // Return if on pantheon/platform servers.
-      if (isset($_ENV['PANTHEON_ENVIRONMENT']) || isset($_ENV['PLATFORM_ENVIRONMENT'])) {
+      if (strpos(shell_exec('which ddev'), 'no ddev') !== FALSE) {
         return;
       }
       
