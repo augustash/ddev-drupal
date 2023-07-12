57be7ae1ea30e9526ad88fe8e052050a645a11d4
KazaJhodo <kazajhodo@ashenrayne.com>
2023-07-12 16:43:33 -0500

    Change back to return.
    Must be another reason why this isn’t working.
    
    Pantheon env must not be set.

--- scripts/ddev.php
+++ scripts/ddev.php
@@ -8,7 +8,7 @@ class Ddev {
     public static function postPackageInstall(Event $event) {
       // Return if on pantheon/platform servers.
       if (isset($_ENV['PANTHEON_ENVIRONMENT']) || isset($_ENV['PLATFORM_ENVIRONMENT'])) {
-        die();
+        return;
       }
       
       $configuration = __DIR__ . '/../../../../.ddev/config.yaml';
