1be7d503543775edecfbe40c6457cad57ce20317
KazaJhodo <kazajhodo@ashenrayne.com>
2023-08-25 16:04:03 -0500

    Change subdomains to askConfirmation.

--- src/Ddev.php
+++ src/Ddev.php
@@ -88,11 +88,13 @@ class Ddev {
       ];
 
       // Subdomain configuration handling.
-      $subdomains = explode(' ', $io->ask('<info>Subdomains? (space delimiter)</info>:' . "\n > "));
-      $config['additional_hostnames'] = [];
+      $subdomains = explode(' ', $io->askConfirmation('<info>Subdomains? (space delimiter) [<comment>no</comment>]</info>:' . "\n > ", FALSE));
+      if ($subdomains) {
+        $config['additional_hostnames'] = [];
 
-      foreach ($subdomains as $subdomain) {
-        $config['additional_hostnames'][] = $subdomain. '.' . $clientCode;
+        foreach ($subdomains as $subdomain) {
+          $config['additional_hostnames'][] = $subdomain. '.' . $clientCode;
+        }
       }
 
       // config.yaml.
